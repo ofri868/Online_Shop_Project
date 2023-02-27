cea9c7214c20483415b4f30b217affa6e3be00a4
Ofri Gal <ofri0101@gmail.com>
2023-02-27 14:35:06 +0200

    first deployment

--- Backend/base/views.py
+++ Backend/base/views.py
@@ -1,4 +1,3 @@
-from turtle import up
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
